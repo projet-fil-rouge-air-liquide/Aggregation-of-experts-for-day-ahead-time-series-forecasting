4394956160a92853f5d0f97e82daa0affe9d85c3
Ambroise012 <ambroise012@Ambroise>
2026-01-23 21:42:08 +0100

    feat: evaluate models

--- src/opera/regime.py
+++ src/opera/regime.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-experts = pd.read_csv("data/experts/experts.csv")
+experts = pd.read_csv("data/processed_data/data_engineering_belgique.csv")
 
 targets = experts.iloc[:, 1]
 dates = experts["Date_Heure"]
@@ -39,4 +39,4 @@ features_scaled = pd.DataFrame(
 for col in features_scaled.columns:
     experts[col] = features_scaled[col]
 
-experts.to_csv("data/experts/experts.csv", index=False)
+experts.to_csv("data/processed_data/data_engineering_belgique_regime.csv", index=False)
---
+++ utils/eval_utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+
+def compute_metrics(y_true, y_pred):
+    mae = mean_absolute_error(y_true, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+    # mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+    r2 = r2_score(y_true, y_pred)
+    return mae, rmse, r2
